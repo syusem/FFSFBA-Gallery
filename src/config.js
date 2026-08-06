07b1fa8e948a1e8ce65960270d747e60940195ca
syusem <syusem@yahoo.com>
2026-08-06 13:43:14 -0700

    Remove duplicate properties in config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
+
 export const config = {
-
-    apiKey : "bbd36ebd13c29c1a18367b1df66106f2",
-
-    userId : "44239813@N00"
+    apiKey: "bbd36ebd13c29c1a18367b1df66106f2",
+    userId: "44239813@N00"
 
 };
 
